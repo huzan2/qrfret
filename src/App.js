31ce12c00f9833c0c8f9a0d0537afefff4237bff
Jihan Park <74421125+bbbjihan@users.noreply.github.com>
2023-11-02 22:53:31 +0900

    gh-page setting

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Router from "./Router";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/qrfret">
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <Logo />
         <Router />
