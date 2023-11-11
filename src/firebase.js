d8ed5706624af0ac1cd0fb5093efc526c76093e6
Jihan Park <74421125+bbbjihan@users.noreply.github.com>
2023-11-12 03:13:41 +0900

    env 설정

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,12 @@ import { getDatabase, ref } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "qrfret.firebaseapp.com",
-  databaseURL:
-    "https://qrfret-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "qrfret",
-  storageBucket: "qrfret.appspot.com",
-  messagingSenderId: "1049968395823",
-  appId: "1:1049968395823:web:f36b2d40a7bbd38da168da",
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
