516ec1fd4e4eaacaccc5c72d2eba5bf88a3a52d5
Jihan Park <74421125+bbbjihan@users.noreply.github.com>
2023-11-13 12:10:55 +0900

    가사 추가하다가 귀찮아서 냅둠

--- src/setList.js
+++ src/setList.js
@@ -20,7 +20,8 @@ export const setList = {
       "songName": "Boat",
       "vocal": [
         "최현석"
-      ]
+      ],
+      // "lyric": "{I'm on a boat\n눈에보이는 사방이 바다야\n갓 잡아올린 생선을 회쳐서먹어\nI'm on a boat\n멀어지는 도시\nWe're going on\n어딘가로 멀리멀리\nI'm on a boat\n내가 원했던건 그게 전부야\n흘러가는파도와 내 흔들리는 보트\nI'm on a boat\n멀어지는 도시\nWe're going on\n어딘가로 멀리멀리}\n\n지는 해를 바라보면서\n잠들수있는 낙이 필요해\n떠나지말란 법은 없지만\n두고가야할게많아 겁이나는걸\n가끔은 한심한 꿈들이 필요해\nBut 나를 바라보는 눈이 너무많아\n저 멀리 갈수있는 배가 필요해\n내 삶은 파도 따라서 흘러가야돼\n\n{I'm on a boat\n눈에보이는 사방이 바다야\n갓 잡아올린 생선을 회쳐서먹어\nI'm on a boat\n멀어지는 도시\nWe're going on\n어딘가로 멀리멀리\nI'm on a boat\n내가 원했던건 그게 전부야\n흘러가는파도와 내 흔들리는 보트\nI'm on a boat\n멀어지는 도시\nWe're going on\n어딘가로 멀리멀리}\n지금은 꽤나 멀리온듯해\n생각했던 모습과는 조금달라도\n괜찮아 이건모두 내가 바랬던\n이상의 일부야 이정돈 감수해야지\n큰 파도가 날 덮칠수도있지만\n뜻밖의 사고가 날 수도있지만\nI'll be ok 난 내 삶에 만족해\n날 향한 걱정과 우려는 필요없어\n\n{I'm on a boat\n눈에보이는 사방이 바다야\n갓 잡아올린 생선을 회쳐서먹어\nI'm on a boat\n멀어지는 도시\nWe're going on\n어딘가로 멀리멀리\nI'm on a boat\n내가 원했던건 그게전부야\n흘러가는파도와 내 흔들리는 보트\nI'm on a boat\n멀어지는 도시\nWe're going on\n어딘가로 멀리멀리\nx2\n}"
     },
     {
       "bass": [
@@ -42,7 +43,8 @@ export const setList = {
       "songName": "Fly away",
       "vocal": [
         "김다솔"
-      ]
+      ],
+      // "lyric" : "하루 종일 엉망이었었어\n일어나서부터 기분이 뭐 같았어\n아침을 먹는데 확인한 핸드폰엔\n밤사이 와있는 네 문잘 보고선 기분을 잡쳤어\n\n또다시 날 찔러보는 네가 뻔히 보여서\n거지 같지 그다지 썩 반갑지 않으니까\n네가 힘들어하는지 어떤지\n이젠 아무렇지도 않아 오 난\n하나도 안 미안해서 미안해\n\n{I'm gonna fly away gonna fly away\n마음은 한결 가볍지\n날아갈 듯 가볍지\nI'll never cry again yeah I'll never cry again\n이제 훌훌 털어버리고}\n\n흔들렸어 솔직히 좋았어 너의 연락이\n외롭고 힘들 때 항상 넌 내 옆에 없었어\n누구보다 소중했던 너 땜에 깨달은 게 하나 있어\n나도 역시 내가 제일 중요해\n\n{I'm gonna fly away gonna fly away\n마음은 한결 가볍지\n날아갈 듯 가볍지\nI'll never cry again yeah I'll never cry again\n이제 훌훌 털어버리고}\n\n매일 밤 너를 그리던 내가\n이젠 더 이상 나는 널 찾지 않아\n왜 억울해? 미안해? 웃기지 말아\n절대 나는 돌아가지 않아\n너도 아팠겠지만 과연\n나만큼이었을까\n언젠가 너를 생각하면 한번 피식\n웃고 말겠지\n\n{Gonna fly away gonna fly away\n마음은 한결 가볍지\n날아갈 듯 가볍지\nI'll never cry again yeah I'll never cry again\n이제 훌훌 털어버리고\nx2}\n\n여름은 모두 끝났지\n다시 오지 않겠지\nI'll never cry again yeah I'll never cry again\n이제 웃을 일만 남았네\n",
     },
     {
       "bass": [
@@ -60,7 +62,8 @@ export const setList = {
       "songName": "Sk8er Boi",
       "vocal": [
         "김민지"
-      ]
+      ],
+      // "lyric" : "He was a boy, She was a girl\nCan I make it anymore obvious?\nHe was a punk. She did ballet.\nWhat more can I say?\nHe wanted her. She'd never tell.\nsecretly she wanted him as well.\nBut all of her friends, Stuck up their nose.\nThey had a problem with his baggy clothes.\n\nHe was a sk8er boi\nshe said see ya later boi.\nHe wasn't good enough for her.\nShe had a pretty face but her head was up in space.\nShe needed to come back down to earth. \n\nFive years from now she sits at home\nFeeding the baby she's all alone.\nShe turns on TV and guess who she sees.\nSk8er boi rocking up MTV.\nShe calls up her friends.\nThey already know\nAnd they've all got tickets to see his show.\nShe tags along, stands in the crowd.\nLooks up at the man that she turned down.\n\nHe was a sk8er boi\nshe said see ya later boi.\nHe wasn't good enough for her.\nNow he's a superstar slamming on his guitar\ndoes your pretty face see what he's worth?\n\nHe was a sk8er boi\nshe said see ya later boi.\nHe wasn't good enough for her.\nNow he's a superstar slamming on his guitar\ndoes your pretty face see what he's worth?\n\nSorry girl but you missed out.\nWell tough luck that boi's mine now.\nWe are more than just good friends.\nThis is how the story ends.\nToo bad that you couldn't see.\nSee the man that boi could be.\nThere is more than meets the eye,\nI see the soul that is inside.\nHe's just a boi, and I'm just a girl.\nCan I make it anymore obvious?\nWe are in love.\nHaven't you heard how we rock each other's world?\n\nI met the sk8er boi I said see ya later boi.\nI'll be backstage after the show.\nI'll be at the studio singing the song we wrote\nabout a girl he use to know.\nI met the sk8er boi I said see ya later boi.\nI'll be backstage after the show.\nI'll be at the studio singing the song he wrote\nabout a girl you used to know\n"
     },
     {
       "bass": [
@@ -81,7 +84,8 @@ export const setList = {
       "songName": "Summer",
       "vocal": [
         "최승혜"
-      ]
+      ],
+      // "lyric" : "(Our eyes are closed but\nwe know when the sun is near us\nWe'll be in love forever\nMy door had been ripped off somehow\nBut now it's constantly fixed by you\nI hate to admit it\n\nDon't you let me slip away\nWhen I fight for silly things\nDon't show your back to me\nWhen we fall down together\n\nDon't you let me get away\nWhen I pretend to be okay\nPromise I'll be true to you\nBut somehow we will realize\nSomeday we'll stand here by the sun\nDon't let me get away)\nx2\n"
     },
     {
       "bass": [
